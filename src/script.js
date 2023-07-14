7a5e78a3ac6b216b5a060118c07c326963d4eecf
Ruth Cedeño <emperatrizcedeno18@gmail.com>
2023-07-13 23:27:45 -0400

    realizacion del innerHTML donde guarda y muestra en pantalla el titulo y las notas guardas en el localStorage

--- src/script.js
+++ src/script.js
@@ -1,18 +1,22 @@
-const note = document.getElementById('note');
+const note = document.querySelector('#note');
 const btnSave = document.querySelector('.btn-save');
+const saveNote = document.querySelector('.save-note');
+const titleNote = document.querySelector('#title-note')
 
 btnSave.addEventListener('click', (e) => {
-    const valueNote = note.value;
-    const savedNotes = localStorage.getItem('notes');
+  const valueTitle = titleNote.value;
+  const valueNote = note.value;
 
-    if (savedNotes){
-        const noteArray = [valueNote]
-        localStorage.setItem('notes', JSON.stringify(noteArray));
-        console.log(noteArray);
-    }
+  if (valueNote !== '') {
+    localStorage.setItem('title', valueTitle);
+    localStorage.setItem('note', valueNote);
+    
+    const loadTitle = localStorage.getItem('title');
+    const loadNote = localStorage.getItem('note');
+    saveNote.innerHTML = `<p id="title-note">${loadTitle}</p>
+    <textarea id="note" cols="50" rows="10" ref="textarea">${loadNote}</textarea>`;
+
+    titleNote.value = '';
+    note.value = '';
+  }
 });
-
-
-
-
-
