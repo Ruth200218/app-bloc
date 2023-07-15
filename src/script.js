9c618e7fe25cef3292ebbaf9f3cacc63215193cb
Ruth Cedeño <emperatrizcedeno18@gmail.com>
2023-07-14 22:17:32 -0400

    realizacion de JSON para los valores del titulo y las notas

--- src/script.js
+++ src/script.js
@@ -6,17 +6,40 @@ const titleNote = document.querySelector('#title-note')
 btnSave.addEventListener('click', (e) => {
   const valueTitle = titleNote.value;
   const valueNote = note.value;
-
+  
   if (valueNote !== '') {
-    localStorage.setItem('title', valueTitle);
-    localStorage.setItem('note', valueNote);
+    const values ={
+      title : `${valueTitle}`,
+      content : `${valueNote}`
+    }
     
-    const loadTitle = localStorage.getItem('title');
-    const loadNote = localStorage.getItem('note');
-    saveNote.innerHTML = `<p id="title-note">${loadTitle}</p>
-    <textarea id="note" cols="50" rows="10" ref="textarea">${loadNote}</textarea>`;
-
+    localStorage.setItem('title', JSON.stringify(values));
+    localStorage.setItem('note', JSON.stringify(values));
+    
+    const loadTitle = JSON.parse(localStorage.getItem('title'));
+    const loadNote = JSON.parse(localStorage.getItem('note'));
+    
+    const valueT = loadTitle.title;
+    const valueN = loadNote.content;
+    
+    saveNote.innerHTML = `<p id="title-note">${valueT}</p>
+    <textarea id="note" cols="50" rows="10" ref="textarea">${valueN}</textarea>`;
+    
+    
     titleNote.value = '';
     note.value = '';
   }
 });
+
+window.onload = function (){
+  if (localStorage.hasOwnProperty('title') && localStorage.hasOwnProperty('note')){
+    const loadTitle = JSON.parse(localStorage.getItem('title'));
+    const loadNote = JSON.parse(localStorage.getItem('note'));
+    
+    const valueT = loadTitle.title;
+    const valueN = loadNote.content;
+    
+    saveNote.innerHTML = `<p id="title-note">${valueT}</p>
+    <textarea id="note" cols="50" rows="10" ref="textarea">${valueN}</textarea>`;
+  }
+}
